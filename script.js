999901806087d20e066a91f7dc3e4dc84322c631
Rairu <rairu.rgomes@gmail.com>
2020-09-26 01:09:44 -0300

    Third Commit

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 function dakrmode() {
 
+    
+    var teste = document.body;
+    var h01 = document.getElementsByTagName("h1");
 
-    document.querySelector(".dark").classList.toggle("tudo-darkmode");
+    teste.classList.toggle("tudo-darkmode");
+
+    for(var i = 0; i< h01.length; i++){
+        h01[i].classList.toggle("tudo-darkmode")
+      }
 }
+
+function resultado(){
+
+    var quilometro = document.getElementById("quilometro").value;
+    var centimetro ;
+    var metro ;
+    metro =  quilometro * 1000 ;
+    centimetro = quilometro * 100000;
+
+    document.getElementById("metro").value = metro;
+    document.getElementById("centimetro").value = centimetro ; 
+
+}
+
+function limpar(){
+    document.getElementById("quilometro").value = "";
+    document.getElementById("centimetro").value = "";
+    document.getElementById("metro").value = "";
+}
+
+    
